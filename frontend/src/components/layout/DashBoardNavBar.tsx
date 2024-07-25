d87ceff7ab52b8749e3ad297d6b38d2dc883e924
bryanleezh <leebryan307@gmail.com>
2024-07-26 00:15:14 +0800

    Add dashboard nav bar

---
+++ frontend/src/components/layout/DashBoardNavBar.tsx
@@ -0,0 +1,76 @@
+import { Link } from "next-view-transitions";
+import { useDashBoardNavBarStore } from "../../../store/DashBoardNavBarState";
+import { Breadcrumb, BreadcrumbList, BreadcrumbItem, BreadcrumbLink, BreadcrumbSeparator } from "../ui/breadcrumb";
+import { Button } from "../ui/button";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+export default function DashBoardNavBar() {
+    // main state of dashboard to display different set of buttons
+    const DashBoardNavBarState = useDashBoardNavBarStore((state) => state.mainState);
+    const setDashBoardNavBarState = useDashBoardNavBarStore((state) => state.setMainState);
+    
+    const pathname = usePathname();
+
+    const generateBreadcrumbs = (pathname: string) => {
+      const pathParts = pathname.split('/').filter(Boolean);
+      return pathParts.map((part, index) => {
+        const href = '/' + pathParts.slice(0, index + 1).join('/');
+        const label = part.charAt(0).toUpperCase() + part.slice(1).replace(/([A-Z])/g, ' $1').trim();
+        return { href, label };
+      });
+    };
+
+    const breadcrumbs = generateBreadcrumbs(pathname);
+
+    // TODO: Add button actions
+    const getButtonSet = (state: string) => {
+        switch (state) {
+            case "Main":
+                return (
+                    <Link href="/dashboard/createclientportfolio">
+                        <Button variant="destructive" className="mr-2">
+                            Create Client Portfolio
+                        </Button>
+                    </Link>
+                );
+
+            case "Portfolio":
+                return (
+                    <>
+                        <Button className="mr-4 bg-blue-400">Portfolio Optimization</Button>
+                        <Button className="mr-4 bg-red-500">Create New Order</Button>
+                        <Button className="mr-4 bg-red-700">Edit Portfolio</Button>
+                    </>
+                );
+            case "Empty":
+                return <></>;
+            default:
+                return null;
+      }
+    };
+
+    return (
+        <nav className="flex items-center justify-between h-16 bg-grey-navbar text-black-font px-24">
+            <div className="flex items-center">
+                <Breadcrumb>
+                    <BreadcrumbList>
+                        {breadcrumbs.map((crumb, index) => (
+                            <React.Fragment key={crumb.href}>
+                                <BreadcrumbItem>
+                                    <Link href={crumb.href} className="text-lg">
+                                        {crumb.label}
+                                    </Link>
+                                </BreadcrumbItem>
+                                {index < breadcrumbs.length - 1 && <BreadcrumbSeparator />}
+                            </React.Fragment>
+                        ))}
+                    </BreadcrumbList>
+                </Breadcrumb>
+            </div>
+            <div>
+                {getButtonSet(DashBoardNavBarState)}
+            </div>
+        </nav>
+    )
+}
--- frontend/src/components/layout/NavBar.tsx
+++ frontend/src/components/layout/NavBar.tsx
@@ -6,34 +6,36 @@ import ubsLogo from "../../../public/ubs-logo.png";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from "../ui/dropdown-menu";
 import { Button } from "../ui/button";
 
+const menuItems = ["Dashboard", "Finance News"];
+
 export default function NavBar() {
     const [username, setUsername] = useState<string>("Chester");
     const pathname = usePathname();
 
     const isLinkActive = (href: string) => pathname === href;
+    const formatMenuItem = (item: string): string => {
+        return item.toLowerCase().replace(/\s+/g, '');
+    };
 
     return (
-        <nav className="flex items-center justify-between h-16 bg-black-navbar text-white px-24">
+        <nav className="flex items-center justify-between h-20 bg-black-navbar text-white px-24">
             <div className="flex items-center">
                 <Image src={ubsLogo} alt="logo" priority className="h-16 w-16"/>
-                <div className="ml-4">
-                    <Link 
-                        href="/dashboard" 
-                        className={`text-lg relative ${isLinkActive('/dashboard') ? 'text-white' : 'text-gray-300'}`}
-                    >
-                        Dashboard
-                        {isLinkActive('/dashboard') && <span className="absolute bottom-0 left-0 w-full h-0.5 bg-white"></span>}
-                    </Link>
-                </div>
-                <div className="ml-4">
-                    <Link 
-                        href="/financenews" 
-                        className={`text-lg relative ${isLinkActive('/financenews') ? 'text-white' : 'text-gray-300'}`}
-                    >
-                        Finance News
-                        {isLinkActive('/financenews') && <span className="absolute bottom-0 left-0 w-full h-0.5 bg-white"></span>}
-                    </Link>
-                </div>
+                {menuItems.map((menuItem, index) => {
+                    const formattedItem = formatMenuItem(menuItem);
+                    return (
+                        <div className="ml-4">
+                            <Link
+                                key={index}
+                                href={`/${formattedItem}`}
+                                className={`text-lg relative ${isLinkActive(`/${formattedItem}`) ? 'text-white' : 'text-gray-300'}`}
+                            >
+                                {menuItem}
+                                {isLinkActive(`/${formattedItem}`) && <span className="absolute bottom-0 left-0 w-full h-0.5 bg-white"></span>}
+                            </Link>
+                        </div>
+                    );
+                })}
             </div>
             <DropdownMenu>
                 <DropdownMenuTrigger asChild>
