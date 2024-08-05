8dee0a56942d5f7e57e9de35605e0d69a2ad0fc6
bryanleezh <leebryan307@gmail.com>
2024-08-06 00:09:02 +0800

    Add delete asset item on order checkout

--- frontend/src/components/dashboard/Portfolio/orderform/OrdersCheckoutCard.tsx
+++ frontend/src/components/dashboard/Portfolio/orderform/OrdersCheckoutCard.tsx
@@ -1,13 +1,16 @@
 import OrderTypeBadge from "@/components/global/OrderTypeBadge";
+import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { Table, TableHeader, TableRow, TableHead, TableBody, TableCell } from "@/components/ui/table";
-import { OrderStockItem } from "@/lib/types";
+import { AssetsItem } from "@/lib/types";
+import { X } from 'lucide-react';
 
 interface OrdersCheckoutCardProps {
-    data: OrderStockItem[];
+    data: AssetsItem[];
+    onDelete: (ticker: string) => void;
 }
 
-export default function OrdersCheckoutCard({ data }: OrdersCheckoutCardProps) {
+export default function OrdersCheckoutCard({ data, onDelete }: OrdersCheckoutCardProps) {
     return (
         <Card className="flex flex-col w-full p-4 gap-2">
             <h2 className="text-xl font-medium">Order Execution Progress</h2>
@@ -20,6 +23,7 @@ export default function OrdersCheckoutCard({ data }: OrdersCheckoutCardProps) {
                         <TableHead>Position | Mkt</TableHead>
                         <TableHead>Last | Cost</TableHead>
                         <TableHead>Order Type</TableHead>
+                        <TableHead>Action</TableHead>
                     </TableRow>
                 </TableHeader>
                 <TableBody>
@@ -47,6 +51,9 @@ export default function OrdersCheckoutCard({ data }: OrdersCheckoutCardProps) {
                             </TableCell>
                             <TableCell>
                                 <OrderTypeBadge orderType={item.orderType} />
+                            </TableCell>
+                            <TableCell>
+                                <Button variant="ghost" className="hover:bg-red-500 hover:text-white" onClick={() => onDelete(item.ticker)}><X /></Button>
                             </TableCell>
                         </TableRow>
                     ))}
