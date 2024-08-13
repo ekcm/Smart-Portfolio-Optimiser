81234d1f3a043684320cf18c4e0820e7ccc117bb
japhethleongyh <japheth.leong.yun.hao@gmail.com>
2024-08-14 00:17:16 +0800

    feat: onboard order module to main app and enable mongoose

--- nestjs-backend/src/app.module.ts
+++ nestjs-backend/src/app.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { portfolioModule } from './module/portfolio.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { OrderModule } from './module/order.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [portfolioModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.DATABASE_CONNECTION_STRING),
+    OrderModule,
+    portfolioModule
+  ],
 })
 
 export class AppModule {}
