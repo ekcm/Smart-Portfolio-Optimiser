21d8511370fafe0d55a6ce2a63dd5ee7626a747c
japhethleongyh <japheth.leong.yun.hao@gmail.com>
2024-08-15 23:01:51 +0800

    feat: onboard swagger

--- nestjs-backend/src/main.ts
+++ nestjs-backend/src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  const config = new DocumentBuilder()
+    .setTitle('Portfolio Optimizer Application API')
+    .setDescription('core application in nestjs')
+    .setVersion('1.0')
+    .addTag('portfolios')
+    .addTag('prders')
+    .build()
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document)
+
   app.useGlobalPipes(new ValidationPipe());
+  
   await app.listen(3000);
 }
 bootstrap();
