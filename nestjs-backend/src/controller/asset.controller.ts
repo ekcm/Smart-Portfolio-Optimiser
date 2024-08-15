77dd0c71b4704cd38d5e1941daaeabd0c3d8ed72
Wei Sheng Ang <aws1@hotmail.sg>
2024-08-15 17:11:36 +0800

    fix: minor formatting changes

--- nestjs-backend/src/controller/asset.controller.ts
+++ nestjs-backend/src/controller/asset.controller.ts
@@ -5,34 +5,34 @@ import { AssetService } from "../service/asset.service";
 
 @Controller("asset")
 export class AssetController {
-  constructor(private readonly assetService: AssetService) { }
+	constructor(private readonly assetService: AssetService) { }
 
-  @Get()
-  async getAll(): Promise<Asset[]> {
-    return await this.assetService.getAll();
-  }
+	@Get()
+	async getAll(): Promise<Asset[]> {
+		return await this.assetService.getAll();
+	}
 
-  @Get('/:ticker')
-  async getById(@Param('ticker') ticker: string): Promise<Asset> {
-    return await this.assetService.getByTicker(ticker);
+	@Get('/:ticker')
+	async getById(@Param('ticker') ticker: string): Promise<Asset> {
+		return await this.assetService.getByTicker(ticker);
 
-  }
+	}
 
-  @Post()
-  async create(@Body() assetDto: AssetDto): Promise<Asset> {
-    return await this.assetService.create(assetDto);
-  }
+	@Post()
+	async create(@Body() assetDto: AssetDto): Promise<Asset> {
+		return await this.assetService.create(assetDto);
+	}
 
-  @Put('/:ticker')
-  async update(@Param('ticker') ticker: string, @Body() assetDto: AssetDto): Promise<Asset> {
-    return await this.assetService.update(ticker, assetDto);
+	@Put('/:ticker')
+	async update(@Param('ticker') ticker: string, @Body() assetDto: AssetDto): Promise<Asset> {
+		return await this.assetService.update(ticker, assetDto);
 
 
-  }
+	}
 
-  @Delete('/:ticker')
-  async delete(@Param('ticker') ticker: string): Promise<void> {
-    await this.assetService.delete(ticker);
+	@Delete('/:ticker')
+	async delete(@Param('ticker') ticker: string): Promise<void> {
+		await this.assetService.delete(ticker);
 
-  }
+	}
 }
