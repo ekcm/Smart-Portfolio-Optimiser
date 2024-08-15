0998139a19374c13119df5486f50a07a5b11a44c
japhethleongyh <japheth.leong.yun.hao@gmail.com>
2024-08-16 01:05:20 +0800

    feat: onboard swagger description

--- nestjs-backend/src/controller/assetprice.controller.ts
+++ nestjs-backend/src/controller/assetprice.controller.ts
@@ -1,40 +1,47 @@
-import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
-import { AssetPriceService } from '../service/assetprice.service';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { ApiOperation } from '@nestjs/swagger';
 import { AssetPriceDto } from '../dto/assetprice.dto';
 import { AssetPrice } from '../model/assetprice.model';
+import { AssetPriceService } from '../service/assetprice.service';
 
 @Controller('assetprice')
 export class AssetPriceController {
-  constructor(private readonly assetPriceService: AssetPriceService) {}
+  constructor(private readonly assetPriceService: AssetPriceService) { }
 
   @Get()
+  @ApiOperation({ summary: 'Get all AssetPrices' })
   async getAll(): Promise<AssetPrice[]> {
     return this.assetPriceService.getAll();
   }
 
   @Get(':ticker')
+  @ApiOperation({ summary: 'Get an array of all AssetPrices with ticker' })
   async getByTicker(@Param('ticker') ticker: string): Promise<AssetPrice[]> {
     return this.assetPriceService.getByTicker(ticker);
   }
 
   @Get(':ticker/:date')
+  @ApiOperation({ summary: 'Get an AssetPrice with ticker and date' })
   async getByTickerAndDate(@Param('ticker') ticker: string, @Param('date') date: string): Promise<AssetPrice> {
     const parsedDate = new Date(date);
     return this.assetPriceService.getByTickerAndDate(ticker, parsedDate);
   }
 
   @Post()
+  @ApiOperation({ summary: 'Create an AssetPrice' })
   async create(@Body() assetPriceDto: AssetPriceDto): Promise<AssetPrice> {
     return this.assetPriceService.create(assetPriceDto);
   }
 
   @Put(':ticker/:date')
+  @ApiOperation({ summary: 'Update an AssetPrice by ticker and date' })
   async update(@Param('ticker') ticker: string, @Param('date') date: string, @Body() updateDto: AssetPriceDto): Promise<AssetPrice> {
     const parsedDate = new Date(date);
     return this.assetPriceService.update(ticker, parsedDate, updateDto);
   }
 
   @Delete(':ticker/:date')
+  @ApiOperation({ summary: 'Delete an AssetPrice by ticker and date' })
   async delete(@Param('ticker') ticker: string, @Param('date') date: string): Promise<void> {
     const parsedDate = new Date(date);
     return this.assetPriceService.delete(ticker, parsedDate);
