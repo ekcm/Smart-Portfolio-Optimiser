59e00bbbd8f877f51e98b3c094e415bfa7f7d491
japhethleongyh <japheth.leong.yun.hao@gmail.com>
2024-08-15 23:08:20 +0800

    feat: onboard in-mem mongo

--- nestjs-backend/src/app.module.ts
+++ nestjs-backend/src/app.module.ts
@@ -1,19 +1,28 @@
 import { Module } from '@nestjs/common';
-import { PortfolioModule } from './module/portfolio.module';
-import { MongooseModule } from '@nestjs/mongoose';
-import { OrderModule } from './module/order.module';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MongoMemoryServer } from 'mongodb-memory-server';
 import { AssetModule } from './module/asset.module';
+import { OrderModule } from './module/order.module';
+import { PortfolioModule } from './module/portfolio.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    MongooseModule.forRoot(process.env.DATABASE_CONNECTION_STRING),
+    MongooseModule.forRootAsync({
+      useFactory: async () => {
+        const mongodb = await MongoMemoryServer.create();
+        const uri = mongodb.getUri();
+        return {
+          uri
+        };
+      },
+    }),
     OrderModule,
     PortfolioModule,
     AssetModule
   ],
 })
-export class AppModule {}
+export class AppModule { }
