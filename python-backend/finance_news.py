d295f16be4468ccd6b83bdb222051d4517de6d10
Elijah Khor <elijahkcm@gmail.com>
2024-08-23 19:16:18 +0800

    hard code sentiment analysis

--- python-backend/finance_news.py
+++ python-backend/finance_news.py
@@ -132,5 +132,62 @@ async def keyword_extraction():
         file.write(json_str)
     return json_obj
 
+@app.get("/sentiment_analysis")
+async def sentiment_analysis():
+    '''
+    This service is used to perform sentiment analysis on the research report.
+    
+    It retrieves the json object from keyword_extraction.json and the entire research text from research_report.txt.
+
+    Using the json object, it performs sentiment analysis on the extracted text.
+    '''
+    with open("keyword_extraction.json", "r") as file:
+        keyword_extraction = file.read()
+        keyword_extraction = json.loads(keyword_extraction)
+
+    with open("research_report.txt", "r") as file:
+        research_report = file.read()
+    
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", "content": "Your role is to search for the sentence or paragraph that is relevant to the keyword."},
+            {"role": "user", "content": f'''
+                ### Instruction ###
+                Extract the sentence or paragraph that is relevant to the keyword.
+                Keywords: "Macy's (M)"
+                Text: {research_report}.
+            '''
+            }
+        ],
+        temperature=0,
+    )
+    keyword_sentence = response.choices[0].message.content
+    print(keyword_sentence)
+
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", "content": "Your role is to perform sentiment analysis on the extracted text."},
+            {"role": "user", "content": f'''
+                ### Instruction ###
+                Perform sentiment analysis on the extracted text. Return the output as Positive, Negative, or Neutral.
+                Text: {keyword_sentence}.
+            '''
+            }
+        ],
+        temperature=0,
+    )
+    sentiment_analysis = response.choices[0].message.content
+    print(sentiment_analysis)
+
+    return_json = {
+        "keyword_extraction": "Macy's (M)",
+        "keyword_sentence": keyword_sentence,
+        "sentiment_analysis": sentiment_analysis
+    }
+
+    return return_json
+
 if __name__ == "__main__":
     uvicorn.run("finance_news:app", host='127.0.0.1', port=5000, reload=True)
