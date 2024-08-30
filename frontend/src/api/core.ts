7d72323d92cc4918d6bbfe5404e0bc08998ebea5
Wei Sheng Ang <aws1@hotmail.sg>
2024-08-30 12:44:39 +0800

    enabled cors

--- frontend/src/api/core.ts
+++ frontend/src/api/core.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export const fetchPortfolios = async () => {
     try {
-        const response = await axios.get('http://localhost:8000/core/' + 'manager')
+        const response = await axios.get('http://localhost:8000/core/' + '66d0424c3261a4419dd55468')
         return response.data
     } catch (error) {
         console.error('Error fetching portfolios:', error)
