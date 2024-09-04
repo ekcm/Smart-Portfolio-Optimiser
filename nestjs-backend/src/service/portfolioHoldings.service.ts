32e3559978558a244c385aa66ae0c6175fe2f80d
Wei Sheng Ang <aws1@hotmail.sg>
2024-09-04 17:33:43 +0800

    refactor assetcalculator

--- nestjs-backend/src/service/portfolioHoldings.service.ts
+++ nestjs-backend/src/service/portfolioHoldings.service.ts
@@ -3,68 +3,43 @@ import { AssetService } from "./asset.service";
 import { AssetPriceService } from "./assetprice.service";
 import { CalculatedPortfolio, PortfolioHoldings } from "src/types";
 import { Portfolio } from "src/model/portfolio.model";
-import { AssetCalculatorService } from "./assetCalculator.service";
+import { CalculatorUtility } from "src/utilities/calculatorUtility";
 
 @Injectable()
 export class PortfolioHoldingsService {
 
-    constructor(private assetService: AssetService, private assetPriceService: AssetPriceService, private assetCalculatorService: AssetCalculatorService) { }
+    constructor(private assetService: AssetService, private assetPriceService: AssetPriceService) { }
 
 
     async getPortfolioHoldings(portfolio: Portfolio, portfolioCalcuations: CalculatedPortfolio): Promise<PortfolioHoldings[]> {
-
-        var name: string;
-        var ticker: string;
-        var type: string;
-        var geography: string;
-        var position: number;
-        var market: number;
-        var last: number;
-        var cost: number;
-        var totalPL: number;
-        var totalPLPercentage: number;
-        var dailyPL: number;
-        var dailyPLPercentage: number;
-        var positionsRatio: number;
 
         const assetHoldings = portfolio.assetHoldings;
         const assetValue = portfolioCalcuations.totalValue - portfolio.cashAmount
         const portfolioHoldings: PortfolioHoldings[] = []
 
         for (var assetHolding of assetHoldings) {
-            ticker = assetHolding.ticker
-            type = assetHolding.assetType
-            cost = assetHolding.cost
-            position = assetHolding.quantity
+            const asset = await this.assetService.getByTicker(assetHolding.ticker)
+            const assetPrice = await this.assetPriceService.getByTickerLatest(assetHolding.ticker)
 
-            const asset = await this.assetService.getByTicker(ticker)
-            name = asset.name
-            geography = asset.geography
-
-            const assetPrice = await this.assetPriceService.getByTickerLatest(ticker)
-            last = assetPrice.todayClose
-            market = await this.assetCalculatorService.calculateMarketValue(assetPrice, assetHolding)
-            totalPL = await this.assetCalculatorService.calculateTotalPL(assetPrice, assetHolding)[0]
-            totalPLPercentage = await this.assetCalculatorService.calculateTotalPL(assetPrice, assetHolding)[1]
-            dailyPL = await this.assetCalculatorService.calculateDailyPL(assetPrice, assetHolding)[0]
-            dailyPLPercentage = await this.assetCalculatorService.calculateDailyPL(assetPrice, assetHolding)[1]
-            positionsRatio = await this.assetCalculatorService.calculatePositionsRatio(assetHolding, assetPrice, assetValue)
-
+            const todayClose = assetPrice.todayClose
+            const yesterdayClose = assetPrice.yesterdayClose
+            const quantity = assetHolding.quantity
+            const cost = assetHolding.cost
 
             portfolioHoldings.push({
-                name: name,
-                ticker: ticker,
-                type: type,
-                geography: geography,
-                position: position,
-                market: market,
-                last: last,
+                name: asset.name,
+                ticker: assetHolding.ticker,
+                type: assetHolding.assetType,
+                geography: asset.geography,
+                position: quantity,
+                market: CalculatorUtility.calculateMarketValue(todayClose, quantity),
+                last: assetPrice.todayClose,
                 cost: cost,
-                totalPL: totalPL,
-                totalPLPercentage: totalPLPercentage,
-                dailyPL: dailyPL,
-                dailyPLPercentage: dailyPLPercentage,
-                positionsRatio: positionsRatio
+                totalPL: CalculatorUtility.totalPL(quantity, cost, todayClose),
+                totalPLPercentage: CalculatorUtility.totalPLPercentage(cost, todayClose),
+                dailyPL: CalculatorUtility.dailyPL(quantity, todayClose, yesterdayClose),
+                dailyPLPercentage: CalculatorUtility.dailyPLPercentage(todayClose, yesterdayClose),
+                positionsRatio: CalculatorUtility.calculatePositionsRatio(todayClose, quantity, assetValue)
             });
         }
 
--- nestjs-backend/src/utilities/calculatorUtility.ts
+++ nestjs-backend/src/utilities/calculatorUtility.ts
@@ -8,4 +8,29 @@ export abstract class CalculatorUtility {
             return + (Math.round(Number(number + "e+" + precision)) + "e-" + precision)
         }
     }
+
+    public static calculateMarketValue(todayClose: number, quantity: number): number {
+        return quantity * todayClose
+    }
+
+    public static totalPL(quantity: number, cost: number, todayClose: number): number{
+        return quantity * (todayClose - cost)
+    }
+
+    public static totalPLPercentage(cost: number, todayClose: number): number {
+        return this.precisionRound((todayClose / cost - 1) * 100, 2) 
+    }
+
+    public static dailyPL(quantity: number, todayClose: number, yesterdayClose: number): number {
+        return quantity * (todayClose - yesterdayClose)
+    }
+
+    public static dailyPLPercentage(todayClose: number, yesterdayClose: number): number {
+        return this.precisionRound((todayClose / yesterdayClose - 1) * 100, 2)
+    }
+
+    public static calculatePositionsRatio(todayClose: number, quantity: number, totalValue: number): number {
+        const marketValue = this.calculateMarketValue(todayClose, quantity)
+        return this.precisionRound(marketValue / totalValue, 2)
+    }
 }
