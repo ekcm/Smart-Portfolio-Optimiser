75b9d70f66f90c474d7a45b462c9941be0c35b74
japhethleongyh <japheth.leong.yun.hao@gmail.com>
2024-09-10 18:58:08 +0800

    fix: portfolio creation module dependencies

--- nestjs-backend/src/module/portfolioCreation.module.ts
+++ nestjs-backend/src/module/portfolioCreation.module.ts
@@ -2,11 +2,13 @@ import { Module } from "@nestjs/common";
 import { AssetPriceModule } from './assetprice.module';
 import { AssetModule } from "./asset.module";
 import { PortfolioCreationService } from "src/service/portfolioCreation.service";
+import { PortfolioCreationController } from "src/controller/portfolioCreation.controller";
 
 @Module({
     imports: [AssetPriceModule, AssetModule],
     providers: [PortfolioCreationService],
     exports: [PortfolioCreationService],
+    controllers: [PortfolioCreationController]
 })
 
 export class PortfolioCreationModule { }
