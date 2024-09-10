916935b1fcb457106876d2b2964777015b7a25c9
japhethleongyh <japheth.leong.yun.hao@gmail.com>
2024-09-10 18:16:30 +0800

    feat: onboard service to generate proposed portfolio

---
+++ nestjs-backend/src/service/portfolioCreation.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { AssetService } from './asset.service';
+import { AssetPriceService } from './assetprice.service';
+import { OrderDto } from 'src/dto/order.dto';
+import { OrderType } from 'src/model/order.model';
+
+@Injectable()
+export class PortfolioCreationService{
+
+    private CASH_PERCENTAGE = 0.1
+
+    constructor(private assetService: AssetService, private assetPriceService: AssetPriceService) {}
+    
+    async generateOrders(cash: number, riskAppetite: string, exclusions: string[], portfolioId: string): Promise<OrderDto[]> {
+        const assets = await this.assetService.getAllExcept(exclusions);
+        const tickers = assets.map(asset => asset.ticker);
+        const weights = {'AAPL': 1}
+        var proposedOrders: OrderDto[] = []
+        for (let ticker in weights) {
+            const assetPrice = await this.assetPriceService.getByTickerLatest(ticker);
+            proposedOrders.push({
+                orderType: OrderType.BUY,
+                orderDate: new Date(),
+                assetName: ticker,
+                quantity: (cash * (1 - this.CASH_PERCENTAGE) * weights[ticker]) / (await assetPrice).todayClose,
+                price: assetPrice.todayClose,
+                portfolioId: portfolioId
+            })
+        }
+
+        return proposedOrders
+    }
+}
+
