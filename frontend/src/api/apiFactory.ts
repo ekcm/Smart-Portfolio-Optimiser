f0c083bf42d477fc2c4b4db938e8149c2a624184
chesterchia <chester.chia@gmail.com>
2024-09-12 23:03:37 +0800

    Update env and add env-example

--- frontend/src/api/apiFactory.ts
+++ frontend/src/api/apiFactory.ts
@@ -1,10 +1,10 @@
-export const BASE_SERVER_URL = process.env.BASE_SERVER_URL || "http://localhost:8000";
+export const BASE_SERVER_URL = process.env.NEXT_PUBLIC_BASE_SERVER_URL || "http://localhost:8000";
 
-export const CORE_API_PATH = process.env.CORE_API_PATH || "/core";
-export const ASSET_API_PATH = process.env.ASSET_API_PATH || "/asset";
-export const ASSET_PRICE_API_PATH = process.env.ASSET_PRICE_API_PATH || "/assetprice";
+export const CORE_API_PATH = process.env.NEXT_PUBLIC_CORE_API_PATH || "/core";
+export const ASSET_API_PATH = process.env.NEXT_PUBLIC_ASSET_API_PATH || "/asset";
+export const ASSET_PRICE_API_PATH = process.env.NEXT_PUBLIC_ASSET_PRICE_API_PATH || "/assetprice";
 
-export const PORTFOLIO_API_PATH = process.env.PORTFOLIO_API_PATH || "/portfolio";
+export const PORTFOLIO_API_PATH = process.env.NEXT_PUBLIC_PORTFOLIO_API_PATH || "/portfolio";
 
-// export const USER_API_PATH = process.env.USER_API_PATH || "/api/v1/user";
-// export const AUTH_API_PATH = process.env.AUTH_API_PATH || "/api/v1/auth";
+// export const USER_API_PATH = process.env.NEXT_PUBLIC_USER_API_PATH || "/api/v1/user";
+// export const AUTH_API_PATH = process.env.NEXT_PUBLIC_AUTH_API_PATH || "/api/v1/auth";
