5681b4f650d444e02820cc33ba2eac78c14190ac
Wei Sheng Ang <aws1@hotmail.sg>
2024-09-16 01:54:27 +0800

    configure port for render

--- nestjs-backend/src/main.ts
+++ nestjs-backend/src/main.ts
@@ -26,7 +26,9 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document)
 
   app.useGlobalPipes(new ValidationPipe());
-  
-  await app.listen(8000);
+
+const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
+
 }
 bootstrap();
