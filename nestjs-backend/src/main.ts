8e77d3b13d9b9f3697e540c93817a1d84280a765
Wei Sheng Ang <aws1@hotmail.sg>
2024-09-16 01:19:08 +0800

    change cors origin url

--- nestjs-backend/src/main.ts
+++ nestjs-backend/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000', // Allow only your frontend origin
+    origin: 'https://fyp-iota-five.vercel.app/', // Allow only your frontend origin
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     credentials: true,
   });
