98cf635a6ec7acb24e4378c960a1bd35313919a5
Wei Sheng Ang <aws1@hotmail.sg>
2024-09-21 21:22:20 +0800

    add or remove cash

--- nestjs-backend/src/service/portfolio.service.ts
+++ nestjs-backend/src/service/portfolio.service.ts
@@ -4,67 +4,107 @@ import { Model } from "mongoose";
 import { PortfolioDto } from "../dto/portfolio.dto";
 import { Portfolio } from "../model/portfolio.model";
 
+export enum HandleCash {
+    ADD = 'ADD',
+    WITHDRAW = 'WITHDRAW'
+}
 @Injectable()
 export class PortfolioService {
-  constructor(@InjectModel(Portfolio.name) private portfolioModel: Model<Portfolio>) { }
+    constructor(@InjectModel(Portfolio.name) private portfolioModel: Model<Portfolio>) { }
 
-  async getAll(): Promise<Portfolio[]> {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve(this.portfolioModel.find().exec());
-      }, 1000);
-    });
-  }
+    async getAll(): Promise<Portfolio[]> {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(this.portfolioModel.find().exec());
+            }, 1000);
+        });
+    }
 
-  async getById(portfolioId: string): Promise<Portfolio> {
-    return new Promise((resolve, reject) => {
-      setTimeout(async () => {
-        const portfolio = await this.portfolioModel.findById(portfolioId).exec();
-        if (portfolio) {
-          resolve(portfolio);
-        } else {
-          reject(new NotFoundException(`Portfolio with ID ${portfolioId} not found`));
-        }
-      }, 1000);
-    });
-  }
+    async getById(portfolioId: string): Promise<Portfolio> {
+        return new Promise((resolve, reject) => {
+            setTimeout(async () => {
+                const portfolio = await this.portfolioModel.findById(portfolioId).exec();
+                if (portfolio) {
+                    resolve(portfolio);
+                } else {
+                    reject(new NotFoundException(`Portfolio with ID ${portfolioId} not found`));
+                }
+            }, 1000);
+        });
+    }
 
-  async create(portfolioDto: PortfolioDto): Promise<Portfolio> {
-    return new Promise((resolve, reject) => {
-      const createdPortfolio = new this.portfolioModel(portfolioDto);
-      setTimeout(async () => {
-        try {
-          resolve(await createdPortfolio.save());
-        } catch (error) {
-          reject(new ConflictException("A portfolio with this portfolioId already exists"))
-        }
-      }, 1000)
-    })
-  }
+    async create(portfolioDto: PortfolioDto): Promise<Portfolio> {
+        return new Promise((resolve, reject) => {
+            const createdPortfolio = new this.portfolioModel(portfolioDto);
+            setTimeout(async () => {
+                try {
+                    resolve(await createdPortfolio.save());
+                } catch (error) {
+                    reject(new ConflictException("A portfolio with this portfolioId already exists"))
+                }
+            }, 1000)
+        })
+    }
 
-  async getByManager(manager: string): Promise<Portfolio[]> {
-    return new Promise((resolve) => {
-      setTimeout(async () => {
-        const portfolios = await this.portfolioModel.find({ manager }).exec();
-        resolve(portfolios);
-      }, 1000)
-    })
-  }
+    async getByManager(manager: string): Promise<Portfolio[]> {
+        return new Promise((resolve) => {
+            setTimeout(async () => {
+                const portfolios = await this.portfolioModel.find({ manager }).exec();
+                resolve(portfolios);
+            }, 1000)
+        })
+    }
 
-  async update(id: string, portfolioDto: PortfolioDto): Promise<Portfolio> {
-    return new Promise((resolve, reject) => {
-        setTimeout(async () => {
-            const existingPortfolio = await this.portfolioModel.findByIdAndUpdate(
-                id,
-                portfolioDto,
-                { new: true }
-            );
-            if (!existingPortfolio) {
-                reject(new NotFoundException('Portfolio #${id} not found'));
-            } else {
-                resolve(existingPortfolio)
-            }
-        }, 1000)
-    })
-}
+    async update(id: string, portfolioDto: PortfolioDto): Promise<Portfolio> {
+        return new Promise((resolve, reject) => {
+            setTimeout(async () => {
+                const existingPortfolio = await this.portfolioModel.findByIdAndUpdate(
+                    id,
+                    portfolioDto,
+                    { new: true }
+                );
+                if (!existingPortfolio) {
+                    reject(new NotFoundException('Portfolio #${id} not found'));
+                } else {
+                    resolve(existingPortfolio)
+                }
+            }, 1000)
+        })
+    }
+
+    async addCash(id: string, cash: number, type: HandleCash): Promise<Portfolio> {
+        return new Promise((resolve, reject) => {
+            setTimeout(async () => {
+
+                if (type === HandleCash.ADD) {
+                    const existingPortfolio = await this.portfolioModel.findByIdAndUpdate(
+                        id,
+                        { $inc: { cashAmount: cash } },
+                        { new: true }
+                    );
+
+                    if (!existingPortfolio) {
+                        reject(new NotFoundException('Portfolio #${id} not found'));
+                    } else {
+                        resolve(existingPortfolio)
+                    }
+                } else {
+                    const existingPortfolio = await this.portfolioModel.findByIdAndUpdate(
+                        id,
+                        { $inc: { cashAmount: -cash } },
+                        { new: true }
+                    );
+
+                    if (!existingPortfolio) {
+                        reject(new NotFoundException('Portfolio #${id} not found'));
+                    } else {
+                        resolve(existingPortfolio)
+                    }
+                }
+
+            }, 1000)
+        })
+    }
+
+
 }
