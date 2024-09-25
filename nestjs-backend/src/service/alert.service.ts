99079bb2abf5fa61314d8e55dc1bbceb4a7f91cb
Wei Sheng Ang <aws1@hotmail.sg>
2024-09-25 20:46:41 +0800

    alerts service

--- nestjs-backend/src/service/alert.service.ts
+++ nestjs-backend/src/service/alert.service.ts
@@ -8,6 +8,18 @@ export class AlertService {
     constructor(private financeNewsService: FinanceNewsService) { }
 
     async getAlerts(tickers: string[]): Promise<AlertDto[]> {
-        return []
+        const alerts: AlertDto[] = []
+        const newses = await this.financeNewsService.getLatestByTickers(tickers)
+        for (const news of newses) {
+            alerts.push({
+                id: news._id.toString(),
+                ticker: news.ticker,
+                date: news.date,
+                sentimentRating: news.sentimentRating,
+                introduction: (news.summary[0].title.toLowerCase() === "introduction") ? news.summary[0].content as string : "No intro text",
+            })
+        }
+
+        return alerts
     }
 }
