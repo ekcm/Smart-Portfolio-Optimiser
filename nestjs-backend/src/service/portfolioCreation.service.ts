f73a7f40a5e04be19c1a7f5cc963e17974a4caf6
japhethleongyh <96232086+japhethleongyh@users.noreply.github.com>
2024-09-26 09:50:28 +0800

    fix: merge conflict

--- nestjs-backend/src/service/portfolioCreation.service.ts
+++ nestjs-backend/src/service/portfolioCreation.service.ts
@@ -1,5 +1,4 @@
-import { Injectable } from '@nestjs/common';
-import { AssetService } from './asset.service';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { AssetPriceService } from './assetprice.service';
 import { OrderDto } from 'src/dto/order.dto';
 import { OrderType } from 'src/model/order.model';
@@ -7,13 +6,17 @@ import { PortfolioService } from './portfolio.service';
 import { OrderStatus } from '../model/order.model';
 import { ProposedPortfolio } from 'src/types';
 import { RiskAppetite } from 'src/model/portfolio.model';
+import { HttpService } from '@nestjs/axios';
+import { lastValueFrom } from 'rxjs';
+import { AssetPrice } from 'src/model/assetprice.model';
 
 @Injectable()
 export class PortfolioCreationService{
 
     private CASH_PERCENTAGE = 0.1
+    private OPTIMIZER_URL=process.env.OPTIMIZER_URL
 
-    constructor(private assetService: AssetService, private assetPriceService: AssetPriceService, private portfolioService: PortfolioService) {}
+    constructor(private assetPriceService: AssetPriceService, private portfolioService: PortfolioService, private httpService: HttpService) {}
     
     async generateOrders(clientName: string, portfolioName: string, riskAppetite: string, cash: number, managerId: string, exclusions: string[]): Promise<ProposedPortfolio> {
         const createdPortfolio = await this.portfolioService.create({
@@ -25,23 +28,32 @@ export class PortfolioCreationService{
             manager: managerId,
             exclusions: exclusions
         })
-        const assets = await this.assetService.getAllExcept(exclusions);
-        const tickers = assets.map(asset => asset.ticker);
-        const weights = {'AAPL': 1}
-        var proposedOrders: OrderDto[] = []
-        for (let ticker in weights) {
-            const assetPrice = await this.assetPriceService.getByTickerLatest(ticker);
-            proposedOrders.push({
-                orderType: OrderType.BUY,
-                orderDate: new Date(),
-                assetName: ticker,
-                quantity: (createdPortfolio.cashAmount * (1 - this.CASH_PERCENTAGE) * weights[ticker]) / assetPrice.todayClose,
-                price: assetPrice.todayClose,
-                portfolioId: createdPortfolio._id.toString(),
-                orderStatus: OrderStatus.PENDING
-            })
+        try {
+            const response = await lastValueFrom(
+                this.httpService.get(this.OPTIMIZER_URL, {
+                    params: {
+                        exclusions: exclusions,
+                    }
+                },
+            ))
+            const weights = response.data
+            var proposedOrders: OrderDto[] = []
+            for (let ticker in weights) {
+                const assetPrice = await this.assetPriceService.getByTickerLatest(ticker);
+                proposedOrders.push({
+                    orderType: OrderType.BUY,
+                    orderDate: new Date(),
+                    assetName: ticker,
+                    quantity: (createdPortfolio.cashAmount * (1 - this.CASH_PERCENTAGE) * weights[ticker]) / assetPrice.todayClose,
+                    price: assetPrice.todayClose,
+                    portfolioId: createdPortfolio._id.toString(),
+                    orderStatus: OrderStatus.PENDING
+                })
+            }
+        } catch (error) {
+            throw new InternalServerErrorException('Optimizer service error')
         }
-
+        
         return {
             portfolioId: createdPortfolio._id.toString(),
             orders: proposedOrders
