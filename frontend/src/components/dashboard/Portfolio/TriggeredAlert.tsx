50bbf93732980aa4bc2fc667c726e510222a0765
Bryan Lee <leebryan307@gmail.com>
2024-10-02 00:02:43 +0800

    Reskin dashboard alerts

--- frontend/src/components/dashboard/Portfolio/TriggeredAlert.tsx
+++ frontend/src/components/dashboard/Portfolio/TriggeredAlert.tsx
@@ -1,6 +1,7 @@
 import { memo } from "react";
 import { Card } from "@/components/ui/card";
 import { Alert } from "@/lib/types";
+import SentimentRatingCustomBadge from "@/components/financenews/SentimentRatingCustomBadge";
 
 
 interface TriggeredAlertProps {
@@ -23,23 +24,24 @@ const TriggeredAlert = memo(function TriggeredAlert({ type, data }: TriggeredAle
     }
 
     return (
-        <Card className="flex flex-col w-full py-4 px-4 bg-red-100 gap-2">
+        <Card className="flex flex-col w-full pt-4 pb-2 px-4 bg-red-100 gap-2">
             <h2 className="text-xl font-medium">Triggered Alerts:</h2>
-            <ul className="list-disc px-2">
+            <ul className="list-disc">
                 {data
                     .filter((item) => item.sentimentRating === 1 || item.sentimentRating === 5)
                     .sort((a, b) => a.sentimentRating - b.sentimentRating)
                     .map((item, index) => (
                         <li
-                            className={`text-gray-600 ${item.sentimentRating === 5 ? "text-green-600" : "text-red-500"}`}
+                            className="text-gray-600"
                             key={index}
                             style={{ listStyleType: 'none' }} 
                         >
-                            <span className="mr-1">
-                                {item.sentimentRating === 5 ? "👍" : "👎"} 
-                            </span>
-                            <a href={`/financenews/${item.id}`} className="hover:underline">
-                                {item.ticker}
+                            <a href={`/financenews/${item.id}`}>
+                                <div className="grid grid-cols-[1fr_auto] items-center gap-4 hover:bg-gray-300 transition-colors p-1">
+                                        {/* TODO: Change this to ticker name for clarity */}
+                                        {item.ticker}
+                                    <SentimentRatingCustomBadge amount={item.sentimentRating} />
+                                </div>
                             </a>
                         </li>
                     ))}
