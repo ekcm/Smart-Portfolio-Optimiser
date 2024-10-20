bf576b02e52744381ade0c974b812bfc5d69d081
bryanleezh <leebryan307@gmail.com>
2024-10-20 20:03:50 +0800

    fix sentiment raitng badge sizing

--- frontend/src/components/dashboard/Portfolio/optimiser/OptimiserChangeList.tsx
+++ frontend/src/components/dashboard/Portfolio/optimiser/OptimiserChangeList.tsx
@@ -49,20 +49,6 @@ export default function OptimiserChangeList({ data, optimisedData, optimisedFlag
                     <OptimisedOrdersCard data={holdingDifferences} optimisedFlag={optimisedFlag} />
                 </div>
             </div>
-            {/* <div className="grid grid-cols-2 gap-4">
-                <div className="flex flex-col gap-2">
-                    <h2 className="text-xl font-medium">Current Portfolio Breakdown</h2>
-                    <Card className="flex flex-col w-full p-4 gap-2 h-80 overflow-y-auto">
-                        <SecuritiesChart data={data.portfolioBreakdown.securities} />
-                    </Card>
-                </div>
-                <div className="flex flex-col gap-2">
-                    <h2 className="text-xl font-medium">Proposed Portfolio Breakdown</h2>
-                    <Card className="flex flex-col w-full p-4 gap-2 h-80 overflow-y-auto">
-                       <OptimisedSecuritiesChart data={mockOptimizer.portfolioBreakdown} optimisedFlag={optimisedFlag} />
-                    </Card>
-                </div>
-            </div> */}
         </div>
     )
 }
