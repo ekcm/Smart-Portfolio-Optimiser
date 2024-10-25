00ad508ec027810f66b00a6e55d883775495ec50
Elijah Khor <elijahkcm@gmail.com>
2024-10-25 13:57:56 +0800

    add trade execution

--- python-backend/report_generator/report.py
+++ python-backend/report_generator/report.py
@@ -2,12 +2,13 @@ from reportlab.lib.pagesizes import A4
 from reportlab.lib import colors
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.lib.units import inch
-from reportlab.platypus import SimpleDocTemplate, Paragraph, Table, TableStyle
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Table, TableStyle, Spacer
 from reportlab.graphics.shapes import Drawing
 from reportlab.graphics.charts.piecharts import Pie
 import json
 
 def generate():
+    # should be replaced with data from api endpoint
     with open("data.json", "r") as f:
         data = json.load(f)
 
@@ -19,15 +20,15 @@ def generate():
 
     # Set up styles for headings and body text
     styles = getSampleStyleSheet()
-    portfolio_heading_style = styles['Heading1']
-    heading_style = styles['Heading2']
+    h1_heading_style = styles['Heading1']
+    h2_heading_style = styles['Heading2']
     body_style = styles['BodyText']
 
     # print(data['portfolio_details']['portfolio_name'])
-    heading = Paragraph(f"Portfolio report for {data['portfolio_details']['portfolio_name']}", portfolio_heading_style)
+    portfolio_report_heading = Paragraph(f"Portfolio report for {data['portfolio_details']['portfolio_name']}", h1_heading_style)
 
     # Porfolio Summary
-    portfolio_summary_heading = Paragraph("Portfolio Summary", heading_style)
+    portfolio_summary_heading = Paragraph("Portfolio Summary", h2_heading_style)
     portfolio_summary_data = data['portfolio_summary']
 
     # asset_allocation
@@ -52,10 +53,10 @@ def generate():
     ]))
 
     # top holdings by weight
-    top_holdings_by_weight_heading = Paragraph("Top Holdings by Weight", heading_style)
+    top_holdings_by_weight_heading = Paragraph("Top Holdings by Weight", h2_heading_style)
     top_holdings_by_weight_data = portfolio_summary_data['top_holdings_by_weight']
 
-    drawing = Drawing(400, 200)
+    top_holdings_drawing = Drawing(400, 200)
     top_holdings_by_weight_pie_chart = Pie()
     top_holdings_by_weight_pie_chart.data = list(top_holdings_by_weight_data.values())
     top_holdings_by_weight_pie_chart.labels = list(top_holdings_by_weight_data.keys())
@@ -66,10 +67,73 @@ def generate():
 
     for i in range(len(top_holdings_by_weight_pie_chart.data)):
         top_holdings_by_weight_pie_chart.slices[i].fillColor = pie_chart_fillColors[i]
-    drawing.hAlign = 'LEFT'
-    drawing.add(top_holdings_by_weight_pie_chart)
+    top_holdings_drawing.hAlign = 'LEFT'
+    top_holdings_drawing.add(top_holdings_by_weight_pie_chart)
 
-    elements = [heading, portfolio_summary_heading, assets_allocation_table, top_holdings_by_weight_heading, drawing]
+    # sector allocation
+    sector_allocation_heading = Paragraph("Sector Allocation", h2_heading_style)
+    sector_allocation_data = portfolio_summary_data['sector_allocation']
+
+    sector_allocation_drawing = Drawing(400, 200)
+    sector_allocation_pie_chart = Pie()
+    sector_allocation_pie_chart.data = list(sector_allocation_data.values())
+    sector_allocation_pie_chart.labels = list(sector_allocation_data.keys())
+    sector_allocation_pie_chart.x = 50
+    sector_allocation_pie_chart.y = 50
+    sector_allocation_pie_chart.width = 150
+    sector_allocation_pie_chart.height = 150
+
+    for i in range(len(sector_allocation_pie_chart.data)):
+        sector_allocation_pie_chart.slices[i].fillColor = pie_chart_fillColors[i]
+    sector_allocation_drawing.hAlign = 'LEFT'
+    sector_allocation_drawing.add(sector_allocation_pie_chart)
+
+    # commentary and market outlook
+    commentary_heading = Paragraph("Commentary and Market Outlook", h2_heading_style)
+    commentary_data = portfolio_summary_data["commentary_and_market_outlook"]
+    commentary = Paragraph(commentary_data, body_style)
+
+    # trade execution
+    spacer = Spacer(1, 0.2 * inch)
+    trade_execution_heading = Paragraph("Trade Execution", h1_heading_style)
+    trade_execution_body = Paragraph("The following are the trade executions for the portfolio.", body_style)
+
+    # trade execution transactions
+    trade_execution_data = data['trade_execution_summary']
+    trade_execution_transactions = trade_execution_data['transactions']
+
+    trade_execution_table_data = [["ID", "Asset Name", "Direction", "Quantity", "Price"]]
+    for i in range(len(trade_execution_transactions)):
+        trade_execution_table_data.append([i+1, trade_execution_transactions[i]['asset'], trade_execution_transactions[i]['direction'], trade_execution_transactions[i]['quantity'], trade_execution_transactions[i]['strike_price']])
+
+    trade_execution_table = Table(trade_execution_table_data)
+    trade_execution_table.hAlign = 'LEFT'
+    trade_execution_table.setStyle(TableStyle([
+        ('BACKGROUND', (0, 0), (-1, 0), colors.red),
+        ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
+        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+        ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+        ('FONTSIZE', (0, 0), (-1, 0), 12),
+        ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
+        ('BACKGROUND', (0, 1), (-1, -1), colors.beige),
+        ('GRID', (0, 0), (-1, -1), 1, colors.black),
+    ]))
+
+    elements = [
+        portfolio_report_heading,
+        portfolio_summary_heading,
+        assets_allocation_table,
+        top_holdings_by_weight_heading,
+        top_holdings_drawing,
+        sector_allocation_heading,
+        sector_allocation_drawing,
+        commentary_heading,
+        commentary,
+        spacer,
+        trade_execution_heading,
+        trade_execution_body,
+        trade_execution_table
+    ]
     pdf.build(elements)
 
 if __name__ == "__main__":
