51c4d9b1b2438d42da2faf10ac8490c481a880ee
chesterchia <chester.chia@gmail.com>
2024-11-07 17:29:13 +0800

    Cash and securities frontend

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -6,7 +6,7 @@ import { Button } from "@/components/ui/button";
 
 import Image from "next/image";
 
-import ubsLogo from "../../public/ubs-logo.png"
+import ubsLogo from "../../public/pct.png"
 import { Link } from "next-view-transitions";
 import { useTransitionRouter } from 'next-view-transitions';
 
