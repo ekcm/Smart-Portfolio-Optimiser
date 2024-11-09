d91ec4c787703414119978d0ffc4a7ed126601ab
Bryan Lee <leebryan307@gmail.com>
2024-11-10 00:26:41 +0800

    Add new alerts + integration

---
+++ frontend/src/components/dashboard/Portfolio/GoodAlerts.tsx
@@ -0,0 +1,58 @@
+import { memo } from "react";
+import { Card } from "@/components/ui/card";
+import { Alert } from "@/lib/types";
+import SentimentRatingCustomBadge from "@/components/financenews/SentimentRatingCustomBadge";
+
+
+interface GoodAlertsProps {
+    type: "dashboard" | "orderForm";
+    data: Alert[];
+}
+
+const GoodAlerts = memo(function GoodAlerts({ type, data }: GoodAlertsProps) {
+    if (data.length === 0) {
+        if (type === "dashboard") {
+            return <></>;
+        } else if (type === "orderForm") {
+            return (
+                <Card className="flex flex-col flex-grow w-full py-4 px-4 bg-red-100 gap-2">
+                    <h2 className="text-xl font-medium">Breached Rules Alerts:</h2>
+                    <h3 className="text-md text-gray-600">No alerts triggered</h3>
+                </Card>
+            );
+        }
+    }
+    const goodAlerts = data.filter((item) => item.sentimentRating === 4 || item.sentimentRating === 5)
+
+    return (
+        <Card className="flex flex-col flex-grow w-full pt-4 pb-2 px-4 bg-red-100 gap-4">
+                <div>
+                    <h2 className="text-xl font-medium">Ticker Alerts:</h2>
+                    {goodAlerts.length === 0 ? 
+                        <h3 className="text-md text-gray-600">No ticker alerts with low sentiment ratings!</h3>
+                    :
+                        <ul className="list-disc">
+                            {goodAlerts
+                                .sort((a, b) => b.sentimentRating - a.sentimentRating)
+                                .map((item, index) => (
+                                    <li
+                                        className="text-gray-600"
+                                        key={index}
+                                        style={{ listStyleType: 'none' }} 
+                                    >
+                                        <a href={`/financenews/${item.id}`}>
+                                            <div className="grid grid-cols-[1fr_auto] items-center gap-4 hover:bg-gray-300 transition-colors p-1">
+                                                    {item.assetName} ({item.ticker})
+                                                <SentimentRatingCustomBadge amount={item.sentimentRating} />
+                                            </div>
+                                        </a>
+                                    </li>
+                                ))}
+                        </ul>
+                    }
+                </div>
+        </Card>
+    );
+});
+
+export default GoodAlerts;
--- frontend/src/components/dashboard/Portfolio/MainPortfolio.tsx
+++ frontend/src/components/dashboard/Portfolio/MainPortfolio.tsx
@@ -5,6 +5,7 @@ import PortfolioAnalysisCard from "./PortfolioAnalysisCard";
 import PortfolioBreakdownCard from "./PortfolioBreakdownCard";
 import PortfolioHoldingsCard from "./PortfolioHoldingsCard";
 import OrderExecutionProgressCard from "./OrderExecutionProgressCard";
+import GoodAlerts from "./GoodAlerts";
 
 interface MainPortfolioProps {
   data: PortfolioData;
@@ -13,11 +14,12 @@ interface MainPortfolioProps {
 export default function MainPortfolio({ data }: MainPortfolioProps) {
     return (
         <div className="flex flex-col justify-center gap-4 pb-8">
-            <TriggeredAlert type="dashboard" data={data.triggeredAlerts} />
+            <TriggeredAlert type="dashboard" data={data.triggeredAlerts} breachedRules={data.breachedRules} />
             <PortfolioAnalysisCard data={data.portfolioAnalysis} />
             <PortfolioBreakdownCard data={data.portfolioBreakdown} />
             <PortfolioHoldingsCard data={data.portfolioHoldings} />
             <OrderExecutionProgressCard data={data.orderExecutionProgress} />
+            <GoodAlerts type="dashboard" data={data.triggeredAlerts} />
         </div>
     )
 }
